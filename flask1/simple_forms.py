d4e09ee638f2e1521b25128404dea45c15e0ae3c
lightnomyus <lightnomyus.official@gmail.com>
2020-11-15 15:40:54 +0700

    already app, able to check registration

--- flask1/simple_forms.py
+++ flask1/simple_forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Email, Length, EqualTo, email_validator, ValidationError
-
+from flask1.models import Doctor
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -9,6 +9,16 @@ class RegistrationForm(FlaskForm):
     password = StringField('Password', validators=[DataRequired()])
     confirm_password = StringField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = Doctor.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username exists')
+
+    def validate_email(self, email):
+        user = Doctor.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email exists')
 
 
 class LoginForm(FlaskForm):
