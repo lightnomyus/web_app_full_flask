782632b58a1fd891ecb6b61d116b44394a3200e4
Gabriel Dicky <lightnomyus.official@gmail.com>
2020-12-15 22:19:19 +0700

    edit to odbc driver

--- flask1/__init__.py
+++ flask1/__init__.py
@@ -10,7 +10,7 @@ app = Flask(__name__, static_url_path='',
             template_folder='templates', )
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = '65f295c5f593a281ba036c81a4ee5212'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://lightnomyus:L1ghtn0myu5@holterserver1.database.windows.net:1433/holter_database1?driver=SQL+Server+Native+Client+10.0'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://lightnomyus:L1ghtn0myu5@holterserver1.database.windows.net:1433/holter_database1?driver=ODBC+Driver+13+for+SQL+Server'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
