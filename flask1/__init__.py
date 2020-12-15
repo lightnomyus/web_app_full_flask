d48249a1e4300d8730949874efbbf8540ede231e
Gabriel Dicky <lightnomyus.official@gmail.com>
2020-12-15 18:34:33 +0700

    kurung kerawal

--- flask1/__init__.py
+++ flask1/__init__.py
@@ -10,7 +10,7 @@ app = Flask(__name__, static_url_path='',
             template_folder='templates', )
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = '65f295c5f593a281ba036c81a4ee5212'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql://lightnomyus:L1ghtn0myu5@holterserver1.database.windows.net/holter_database1?driver=ODBC Driver 17 for SQL Server'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql://lightnomyus:L1ghtn0myu5@holterserver1.database.windows.net/holter_database1?driver={ODBC Driver 17 for SQL Server}'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
